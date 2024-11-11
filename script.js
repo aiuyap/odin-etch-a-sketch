13f505c5e18774bb1b295e544680133614ab4332
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-11 13:07:57 +0800

    Added eventListeners to each square
    - changes background color when mouse hovers

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const square = document.createElement("div");
 square.classList.add("squareStyle");
 
 for (let i = 0; i < 256; i++) {
+
     boxContainer.appendChild(square.cloneNode());
+}
+
+const allSquare = document.querySelectorAll(".squareStyle");
+
+for (let i = 0; i < allSquare.length; i++) {
+    allSquare[i].addEventListener("mouseover", () => {
+        allSquare[i].style["background-color"] = "#F2613F";
+    });
 }
