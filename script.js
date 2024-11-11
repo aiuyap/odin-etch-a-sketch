06e18bcdc456e4a1d80ee979e524af90d94a73fd
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-11 15:58:31 +0800

    Succesfully added functionality to change size btn

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let sideMaxCount = 16;
-const squareSize = 800 / sideMaxCount; //container is 800px per side
-let totalBoxes = sideMaxCount * sideMaxCount;
+let getSquareSize = (side) => 800 / side; //container is 800px per side
+let getTotalBoxes = (side) => side * side;
 
+let sideMaxCount = 16;
+let squareSize = getSquareSize(sideMaxCount) 
+let totalBoxes = getTotalBoxes(sideMaxCount);
 
 createBoxes(totalBoxes, squareSize);
 
-
+const changeSizeBtn = document.querySelector("#change-size");
+changeSizeBtn.addEventListener("click", () => {
+    const numOfSide = getSize();
+    const squareSize = getSquareSize(numOfSide);
+    const totalBoxes = getTotalBoxes(numOfSide);
+    clearContainer();
+    createBoxes(totalBoxes, squareSize);
+});
 
 function createBoxes (numOfBox, squareWH) {
     const boxContainer = document.querySelector("#container");
@@ -34,4 +43,25 @@ function addStyleToBoxes () {
 function changeColor (box) {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     box.style["background-color"] = `#${randomColor}`;
+}
+
+function getSize () {
+    const getInput = + prompt("Select how many tiles per side (e.g 16 will be 16x16). Maximum of 100:")
+
+    if (Number.isInteger(getInput) && getInput <= 100) {
+        return getInput
+    }
+    else {
+        alert("Invalid input. Please enter a number from 1-100");
+        return getSize();
+    }
+}
+
+function clearContainer () {
+    const container = document.querySelector("#container");
+    const boxes = document.querySelectorAll(".square-style");
+    
+    for(let i = 0; i < boxes.length; i++) {
+        container.removeChild(boxes[i]);
+    }
 }
